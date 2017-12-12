0f0a9f606c301d72aa1184330197f78017c56acb
andy <andy@ashall.co>
2017-12-12 13:55:19 +0000

    tensorflow model

--- dnn.py
+++ dnn.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from keras.preprocessing import image
-from keras.applications.xception import Xception
-from keras.models import Model
-from keras.layers import Dense, Dropout, Flatten
-from keras.callbacks import EarlyStopping
+# from keras.applications.xception import Xception
+# from keras.models import Model
+# from keras.layers import Dense, Dropout, Flatten
+# from keras.callbacks import EarlyStopping
 import os
 
 def normalize(arr):
@@ -56,50 +57,30 @@ X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=.3, random_s
 
 print(y_test.shape)
 
-base_model = Xception(
-  weights = 'imagenet',
-  include_top=False,
-  input_shape=(80, 80, 3)
-)
+feature_columns = [tf.feature_column.numeric_column("x", shape=x.shape)]
 
 num_class = len(set(y))
 
-print(num_class)
-
-# Add a new top layer
-out = base_model.output
-out = Flatten()(out)
-predictions = Dense(num_class, activation='softmax')(out)
-
-# This is the model we will train
-model = Model(inputs=base_model.input, outputs=predictions)
-
-# First: train only the top layers (which were randomly initialized)
-for layer in base_model.layers:
-    layer.trainable = False
-
-model.compile(
-  loss='categorical_crossentropy', 
-  optimizer='adam', 
-  metrics=['accuracy']
+model = tf.estimator.DNNClassifier(
+  hidden_units  = [10, 20, 10],
+  feature_columns=feature_columns,
+  n_classes=num_class,
 )
 
-callbacks_list = [EarlyStopping(monitor='val_acc', patience=3, verbose=1)]
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": X_train},
+    y=y_train,
+    num_epochs=None,
+    shuffle=True)
 
-model.summary()
+model.train(train_input_fn, steps=2000)
 
-model.fit(X_train, y_train, epochs=1, validation_data=(X_test, y_test), verbose=1)
+test_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": X_test},
+    y=y_test,
+    num_epochs=1,
+    shuffle=False)
 
-preds = model.predict(X_test, verbose=1)
+accuracy_score = model.evaluate(input_fn=test_input_fn)["accuracy"]
 
-acc = accuracy_score(y_test, preds)
-
-print(acc)
-
-# sub = pd.DataFrame(preds)
-
-# sub.columns = one_hot.columns.values
-
-# sub['id'] = test_ids
-
-print(sub.head(5))
+print(f'Acc: {accuracy_score}')
