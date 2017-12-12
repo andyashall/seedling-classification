ac4596b861b2613624061777e749c815dcb58c39
andy <andy@ashall.co>
2017-12-12 13:40:06 +0000

    keras

--- dnn.py
+++ dnn.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from keras.preprocessing import image
 from keras.applications.xception import Xception
 from keras.models import Model
@@ -20,7 +21,7 @@ def normalize(arr):
 
 train_path = './data/train/'
 test_path = './data/ten/'
-size = (60, 60)
+size = (80, 80)
 
 x = []
 y = []
@@ -43,11 +44,62 @@ for label in os.listdir(train_path):
 
 print(labels)
 
-targets_series = pd.Series(y)
+targets_series = pd.factorize(pd.Series(y))
 
 x = np.array(x, np.float32)
-y = np.array(pd.factorize(targets_series)[0], np.int_)
+y = np.array(targets_series[0], np.int_)
 
 print(x.shape)
 print(y.shape)
 
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=.3, random_state=1001)
+
+print(y_test.shape)
+
+base_model = Xception(
+  weights = 'imagenet',
+  include_top=False,
+  input_shape=(80, 80, 3)
+)
+
+num_class = len(set(y))
+
+print(num_class)
+
+# Add a new top layer
+out = base_model.output
+out = Flatten()(out)
+predictions = Dense(num_class, activation='softmax')(out)
+
+# This is the model we will train
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# First: train only the top layers (which were randomly initialized)
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(
+  loss='categorical_crossentropy', 
+  optimizer='adam', 
+  metrics=['accuracy']
+)
+
+callbacks_list = [EarlyStopping(monitor='val_acc', patience=3, verbose=1)]
+
+model.summary()
+
+model.fit(X_train, y_train, epochs=1, validation_data=(X_test, y_test), verbose=1)
+
+preds = model.predict(X_test, verbose=1)
+
+acc = accuracy_score(y_test, preds)
+
+print(acc)
+
+# sub = pd.DataFrame(preds)
+
+# sub.columns = one_hot.columns.values
+
+# sub['id'] = test_ids
+
+print(sub.head(5))
