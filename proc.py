6cb4a51a9890a75293e43900bd1489c8e4495ac9
andy <andy@ashall.co>
2017-12-15 16:30:12 +0000

    pre proccesing

--- proc.py
+++ proc.py
@@ -2,7 +2,7 @@ from keras.preprocessing import image
 from PIL import ImageFilter
 import numpy as np
 
-img = image.load_img('./data/test/00c47e980.png')
+img = image.load_img('./data/test/0ad9e7dfb.png')
 
 # img = img.filter(ImageFilter.FIND_EDGES)
 
@@ -16,13 +16,40 @@ img = np.array(image.img_to_array(img))
 # Normalize
 img *= 255.0/img.max()
 
-print(img.shape)
-
 # if green is less than 80 then 0
-img[img[:, :, 1] < 80] = 0
+# img[np.all([img[:, :, 1] < 70, img[:, :, 2] > 60])] = 0
+
+# Link for pre implemented
+
+green = img[:, :, 1] < 100
+
+blue = img[:, :, 2] > 20
+
+g2 = img[:, :, 1] > 100
+
+b2 = img[:, :, 2] > 100
+
+print(green.shape)
+
+new = np.zeros(shape=green.shape, dtype=bool)
+
+for i in range(0, len(green)):
+  for n in range(0, len(green)):
+    if (green[i,n] and blue[i,n]) or (g2[i,n] and b2[i,n]):
+      new[i,n] = True
+      print(new[i,n])
+    else:
+      new[i,n] = False
+      print(new[i,n])
+
+print(new)
+
+print(blue)
+
+img[new] = 0
 
 # if blue is more than 50 then 0
-img[img[:, :, 2] > 50] = 0
+# img[img[:, :, 2] > 60] = 0
 
 # for c in range(img.shape[0]):
 #   for r in range(img.shape[1]):
@@ -35,6 +62,6 @@ img[img[:, :, 2] > 50] = 0
 
 img = image.array_to_img(img)
 
-img = img.filter(ImageFilter.FIND_EDGES)
+# img = img.filter(ImageFilter.FIND_EDGES)
 
 img.save('./data/proc.png', 'png')
